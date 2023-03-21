b5d9d3865177306f099b4091a21413700d283bac
Lucas Gerbasi <126926247+lucasgerbasi@users.noreply.github.com>
2023-03-21 02:54:11 -0300

    Mudança nas regras

---
+++ expand.js
@@ -0,0 +1,25 @@
+const headerBtn = document.querySelector('#alimentos-btn');
+const content = document.querySelector('#content1');
+
+headerBtn.addEventListener('click', () => {
+  const expanded = headerBtn.getAttribute('aria-expanded') === 'true' || false;
+  headerBtn.setAttribute('aria-expanded', !expanded);
+  content.hidden = expanded;
+});
+
+const headerBtn2 = document.querySelector('#pontuacao-btn');
+const content2 = document.querySelector('#content2');
+
+headerBtn2.addEventListener('click', () => {
+  const expanded = headerBtn2.getAttribute('aria-expanded') === 'true' || false;
+  headerBtn2.setAttribute('aria-expanded', !expanded);
+  content2.hidden = expanded;
+});
+const headerBtn3 = document.querySelector('#sangue-btn');
+const content3 = document.querySelector('#content3');
+
+headerBtn3.addEventListener('click', () => {
+  const expanded = headerBtn3.getAttribute('aria-expanded') === 'true' || false;
+  headerBtn3.setAttribute('aria-expanded', !expanded);
+  content3.hidden = expanded;
+});
